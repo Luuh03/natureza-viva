9e6a50a90e68d1d2df558cb355ef49b993e4361d
Luan Queiroz <luan.queiroz2003@gmail.com>
2023-05-26 16:05:04 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title></title>
+        <link type="text/css" rel="stylesheet" href="./styles/style.css"/>
+    </head>
+    <body>
+
+
+    <div class="page">
+        <div class="logo_container">
+            <img src="./images/landing_page_image.png" alt="logo">
+        </div>
+
+        <div class="login_container">
+            <form  action="">
+                <p>Login:</p>
+                <input type="text">
+                <p>Senha:</p>
+                <input type="text">
+
+                <input type="submit" id="button">
+            </form>
+        </div>
+    </div>
+
+        
+    
+
+    </body>
+</html>
