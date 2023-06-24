dde41fd6e5db212ddb0fc92755175f7e7c25e0ba
Ana Jesus <anahbeatriz2001@gmail.com>
2023-06-24 20:03:24 -0300

    alteraçoes homepageadmin

---
+++ pages/homepage_admin.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title></title>
+        <link type="text/css" rel="stylesheet" href="../styles/style.css" />
+        <link type="text/css" rel="stylesheet" href="../styles/style_homepage_admin.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a><img src="../images/logo natu 1.png" alt="logo"></a></li>
+                    <li><a class="" href="">Gerenciar Aluguel</a></li>
+                    <li><a href="">Gerenciar Horários</a></li>
+                    <li><a href="">Gerenciar Espaços</a></li>
+                    <li><a href="">Consultar Agendamentos</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <h1>Olá Administrador</h1><br>
+
+            <h2>Agendamentos aguardando confirmação:</h2>
+
+            <div class="box">
+                <table>
+                    <tr>
+                        <td><h3> Auditório</h3></td>
+                        <td> Rua:</td>
+                    </tr>
+                    <tr>
+                        <td>Dia e Horário>
+                    </tr>
+                    <tr>
+                        <td>Alugado por</td>
+                    </tr>
+                </table> 
+            </div>
+
+            <h2>Agendamentos em aberto:</h2>
+            
+            <div class="box">
+                <table>
+                    <tr>
+                        <td><h3> Auditório</h3></td>
+                        <td> Rua:</td>
+                    </tr>
+                    <tr>
+                        <td>Dia e Horário>
+                    </tr>
+                    <tr>
+                        <td>Alugado por</td>
+                    </tr>
+                </table> 
+            </div>
+        </main>
+    </body>
+
+</html>
